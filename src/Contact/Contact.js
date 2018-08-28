e1eb04d8f2cde0355a9c339b4bf6271291f3aa80
zoltanalmodo <zoltanalmodo@gmail.com>
2018-08-28 04:07:40 +0100

    Update Contact.js
    updated contact information from external websites
    manyhandslondon.co.uk
    shapescollective.com

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,7 +6,27 @@ export default class Contact extends React.Component {
         return (
             <div className="container-contact">
                 <div className="flex-item-contact">
-                    Contact form or contact info
+                    Contact info: 
+                                Many Hands London
+            
+                                Visit Us!
+                                100 Clements Road
+                                London SE16 4DG
+            
+                                Contact:
+                                info@manyhandslondon.co.uk
+                                0207 101 4669
+            
+                                First Name:
+                                Last Name:
+                                Email Address:
+                                Password:
+            
+                                +++Sign Up+++         ==>JOIN<==
+                                
+                                
+                                Many Hands is part of the Shapes Collective.
+                                www.shapescollective.com
                 </div>
             </div>
         );
