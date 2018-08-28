f865ce7a964d03bbdab831c79f7beb4f8577c08b
albertpai <verdant.light@gmail.com>
2018-08-28 02:25:24 +0100

    Create Contact.js component and Contact.less

---
+++ src/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contact.less';
+
+export default class Contact extends React.Component {
+    render() {
+        return (
+            <div className="container-contact">
+                <div className="flex-item-contact">
+                    Contact form or contact info
+                </div>
+            </div>
+        );
+    }
+}
