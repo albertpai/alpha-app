27c90262a020347f4c0864e5d1b99a45f09b4c36
albertpai <verdant.light@gmail.com>
2018-08-30 12:52:32 +0100

    Merge Contact.js and move contact details from FAQ to Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -14,7 +14,7 @@ export default class Contact extends React.Component {
             typeformEmbed.makeWidget(this.element, "https://adam1238.typeform.com/to/Mw8MZU", {
                 hideHeaders: true,
                 hideFooter: true,
-                opacity: 50
+                opacity: 90
             });
         }
     }
@@ -22,8 +22,16 @@ export default class Contact extends React.Component {
     render() {
         return (
             <div className="container-contact">
-                <div className="flex-item-contact">
-
+                <div>
+                    <h2 className="flex-item-contact-title">Contact Us</h2>
+                    <div className="flex-item-contact-form" ref={(element) => this.element = element} />    
+                    <br/>
+                    <p className="flex-item-contact-details">
+                    Our contact details: <br/>
+                    Many Hands London, 100 Clements Road, London SE16 4DG<br/>
+                    Email: info@manyhandslondon.co.uk<br/>
+                    Phone: 0207 101 4669
+                    </p>
                 </div>
             </div>
         );
--- src/FAQ/FAQ.js
+++ src/FAQ/FAQ.js
@@ -8,21 +8,16 @@ export default class FAQ extends React.Component {
                 <h2 className="flex-item-faq-title">FAQ</h2>
                 <div className="flex-item-faq-text">
                     <p>
-                        1) How to visit the site?
+                        1) Question 1
                     <br />
-                        You can visit us at this address:
-                        Many Hands London, 100 Clements Road, London SE16 4DG
                     </p>
                     <p>
-                        2) How to contact Many Hands London?
+                        2) Question 2
                     <br />
-                        Email: info@manyhandslondon.co.uk
-                        Phone: 0207 101 4669
                     </p>
                     <p>
-                        3) Who is Many Hands London?
+                        3) Question 3
                     <br />
-                        Many Hands is part of the <a href="https://www.shapescollective.com/">Shapes Collective</a>.
                     </p>
                 </div>
             </div>
